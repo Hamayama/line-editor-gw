cbb0243c6d70563b43cff0541d1369bd6f1b4f11
Hamayama <hamay1010@gmail.com>
2017-09-24 15:01:44 +0900

    msjis.scm update

--- msjis.scm
+++ msjis.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; msjis.scm
-;; 2017-9-6 v1.60
+;; 2017-9-24 v1.69
 ;;
 ;; ＜内容＞
 ;;   Windows のコマンドプロンプトで Gauche を使うときに、
@@ -11,10 +11,9 @@
 ;;   https://github.com/Hamayama/msjis
 ;;
 (define-module msjis
-  (use gauche.charconv)
   (use gauche.vport)
   (use gauche.uvector)
-  (use gauche.sequence) ; for-each-with-index用
+  (use gauche.charconv)
   (use os.windows)
   (export
     msjis-mode
@@ -53,46 +52,91 @@
     (with-module os.windows sys-read-console-w)
     (with-module os.windows sys-read-console)))
 
+;; ポート属性の設定手続き(Gauche v0.9.4 以後に存在)
+(define port-attribute-set!
+  (if (global-variable-bound? 'gauche 'port-attribute-set!)
+    (with-module gauche port-attribute-set!)
+    (lambda (port key val) #f)))
+
 
 
 ;; 1文字入力の変換処理(内部処理用)
 (define (make-msjis-getc port hdl-type ces ces2 use-api)
   (if use-api
     ;; Windows API 使用のとき
-    (make-msjis-getc-sub port hdl-type 'UTF-16LE ces2 #t 4 2 2)
+    (make-msjis-getc-sub1 hdl-type 'UTF-16LE ces2 4 2 2)
     ;; Windows API 未使用のとき
-    (make-msjis-getc-sub port hdl-type ces       ces2 #f 6 0 1)))
+    (make-msjis-getc-sub2 port ces ces2 6 1)))
 
-;; 1文字入力の変換処理サブ(内部処理用)
-(define (make-msjis-getc-sub port hdl-type ces ces2 use-api maxbytes extrabytes readbytes)
+;; 1文字入力の変換処理サブ1 (Windows API 使用)(内部処理用)
+(define (make-msjis-getc-sub1 hdl-type ces ces2 maxbytes extrabytes readbytes)
+  (define line-start-flag #t)
   ;; 手続きを作って返す
   (lambda ()
-    (rlet1 chr #\null
-      ;; ReadConsole がバッファサイズより1バイト多く書き込む件の対策
-      (let ((buf (make-u8vector (+ maxbytes extrabytes) 0))
-            (hdl (if use-api (sys-get-std-handle hdl-type) #f))
-            (i   0))
-        ;; 文字が完成するまで指定バイトずつ読み込む
-        (let loop ()
-          (if (if use-api
-                (zero? (sys-read-console hdl (uvector-alias <u8vector> buf i (+ i readbytes))))
-                (eof-object? (read-block! buf port i (+ i readbytes))))
-            ;; ファイル終端(EOF)のとき
-            (begin
-              ;(debug-print-str "[EOF]")
-              (set! chr (eof-object)))
-            ;; ファイル終端(EOF)以外のとき
-            ;; 文字コードの変換(外部コード→内部コード)
-            (let1 str (ces-convert (u8vector->string buf 0 (+ i readbytes)) ces ces2)
-              (guard (ex ((<error> ex)
-                          ;; 文字が未完成のとき
-                          (when (< (+ i readbytes) maxbytes)
-                            (set! i (+ i readbytes))
-                            (loop))))
-                ;; 文字が完成したとき
-                (set! chr (string-ref str 0))))))
-        ;(debug-print-buffer (u8vector-copy buf 0 (+ i readbytes)))
-        ))))
+    ;; ReadConsole がバッファサイズより1バイト多く書き込む件の対策
+    (let ((buf (make-u8vector (+ maxbytes extrabytes) 0))
+          (hdl (sys-get-std-handle hdl-type)))
+      ;; 文字が完成するまで指定バイトずつ読み込む
+      (let loop ((i1 0) (i2 readbytes))
+        (if (zero? ($ sys-read-console hdl
+                      (uvector-alias <u8vector> buf i1 i2)))
+          ;; ファイル終端(EOF)のとき
+          (begin
+            ;(debug-print-str "[EOF]")
+            (eof-object))
+          ;; ファイル終端(EOF)以外のとき
+          ;; 文字コードの変換(外部コード→内部コード)
+          (let1 str (ces-convert (u8vector->string buf 0 i2) ces ces2)
+            (guard (ex ((<error> ex)
+                        ;; 文字が未完成のとき
+                        (if (< i2 maxbytes)
+                          (loop i2 (+ i2 readbytes))
+                          (begin
+                            ;(debug-print-str "[Invalid]")
+                            ;(debug-print-buffer (u8vector-copy buf 0 i2))
+                            #\null))))
+              ;; 文字が完成したとき
+              (let1 chr (string-ref str 0)
+                (cond
+                 ;; 行頭のCtrl-zのとき
+                 ((and line-start-flag (eqv? chr #\x1a))
+                  ;(debug-print-str "[EOF](Ctrl-z)")
+                  (eof-object))
+                 ;; その他の文字のとき
+                 (else
+                  (set! line-start-flag (eqv? chr #\newline))
+                  ;(debug-print-char-code chr)
+                  ;(debug-print-buffer (u8vector-copy buf 0 i2))
+                  chr))))))))))
+
+;; 1文字入力の変換処理サブ2 (Windows API 未使用)(内部処理用)
+(define (make-msjis-getc-sub2 port ces ces2 maxbytes readbytes)
+  ;; 手続きを作って返す
+  (lambda ()
+    (let1 buf (make-u8vector maxbytes 0)
+      ;; 文字が完成するまで指定バイトずつ読み込む
+      (let loop ((i1 0) (i2 readbytes))
+        (if (eof-object? (read-block! buf port i1 i2))
+          ;; ファイル終端(EOF)のとき
+          (begin
+            ;(debug-print-str "[EOF]")
+            (eof-object))
+          ;; ファイル終端(EOF)以外のとき
+          ;; 文字コードの変換(外部コード→内部コード)
+          (let1 str (ces-convert (u8vector->string buf 0 i2) ces ces2)
+            (guard (ex ((<error> ex)
+                        ;; 文字が未完成のとき
+                        (if (< i2 maxbytes)
+                          (loop i2 (+ i2 readbytes))
+                          (begin
+                            ;(debug-print-str "[Invalid]")
+                            ;(debug-print-buffer (u8vector-copy buf 0 i2))
+                            #\null))))
+              ;; 文字が完成したとき
+              (let1 chr (string-ref str 0)
+                ;(debug-print-char-code chr)
+                ;(debug-print-buffer (u8vector-copy buf 0 i2))
+                chr))))))))
 
 
 
@@ -112,36 +156,42 @@
      (gauche.ces.utf8
       ;; Windows API が Unicode 版のとき
       ;; (サロゲートペアの文字の折り返しの不具合対策)
-      (let* ((cinfo (sys-get-console-screen-buffer-info hdl))
-             (w     (+ 1 (- (slot-ref cinfo 'window.right)
-                            (slot-ref cinfo 'window.left))))
-             (i1    0))
-        (for-each-with-index
-         (lambda (i2 c)
-           (when (>= (char->integer c) #x10000)
-             (sys-write-console hdl (string-copy str i1 i2))
-             (set! i1 i2)
-             (let* ((cinfo (sys-get-console-screen-buffer-info hdl))
-                    (x     (slot-ref cinfo 'cursor-position.x))
-                    (y     (slot-ref cinfo 'cursor-position.y)))
-               (when (> x (- w 4))
-                 (sys-set-console-cursor-position hdl (- w 1) y)
-                 (sys-write-console hdl " ")))))
-         str)
-        (sys-write-console hdl (string-copy str i1))))
+      (let1 buf (string->u32vector str)
+        (if (u32vector-range-check buf 0 #xffff)
+          ;; サロゲートペアの文字があるとき
+          (let* ((cinfo (sys-get-console-screen-buffer-info hdl))
+                 (w     (+ 1 (- (slot-ref cinfo 'window.right)
+                                (slot-ref cinfo 'window.left))))
+                 (cw    (if (= (sys-get-console-output-cp) 65001) 2 4))
+                 (len   (string-length str)))
+            (let loop ((i1 0) (i2 0))
+              (when (>= (u32vector-ref buf i2) #x10000)
+                (sys-write-console hdl (string-copy str i1 i2))
+                (set! i1 i2)
+                (let* ((cinfo (sys-get-console-screen-buffer-info hdl))
+                       (x     (slot-ref cinfo 'cursor-position.x))
+                       (y     (slot-ref cinfo 'cursor-position.y)))
+                  (when (> x (- w cw))
+                    (sys-set-console-cursor-position hdl (- w 1) y)
+                    (sys-write-console hdl " "))))
+              (if (< (+ i2 1) len)
+                (loop i1 (+ i2 1))
+                (sys-write-console hdl (string-copy str i1)))))
+          ;; サロゲートペアの文字がないとき
+          (sys-write-console hdl str))))
      (else
       ;; Windows API が ANSI 版のとき
       ;; (文字コードの変換が必要)
-      (set! str (ces-convert str ces2 ces))
-      (sys-write-console hdl str))))
+      (sys-write-console hdl (ces-convert str ces2 ces)))))
   ;; 手続きを作って返す
   (lambda (str/char)
-    (let ((str (x->string str/char))
-          (hdl (sys-get-std-handle hdl-type)))
+    (let* ((str (x->string str/char))
+           (hdl (sys-get-std-handle hdl-type))
+           (len (string-length str)))
       ;; 指定文字数ずつ書き出す
       (let loop ((i 0))
         (cond
-         ((<= (string-length str) (+ i maxchars))
+         ((<= len (+ i maxchars))
           (sys-write-console-sub hdl (string-copy str i)))
          (else
           (sys-write-console-sub hdl (string-copy str i (+ i maxchars)))
@@ -163,19 +213,25 @@
 
 
 ;; 変換用パラメータの取得(内部処理用)
-(define (get-msjis-param rmode hdl-type ces use-api stdin-flag)
+(define (get-msjis-param rmode hdl-type ces use-api)
   ;; 文字エンコーディングのチェック
   (define (check-ces ces1 ces2 ces-err)
     (unless (ces-conversion-supported? ces1 ces2)
       (errorf "ces \"~a\" is not supported" ces-err)))
+  ;; 引数のチェック
+  (unless (integer? rmode)
+    (error "integer required, but got" rmode))
   ;; 変換用パラメータの取得
-  (let* ((rdir (redirected-handle? (sys-get-std-handle hdl-type)))
-         (conv (if rdir (if (or (= rmode 2) (= rmode 3)) #t #f) #t))
-         (crlf (if rdir (if (or (= rmode 1) (= rmode 3)) #t #f) #f))
-         (ces2 (gauche-character-encoding)))
+  (let* ((stdin-flag (eqv? hdl-type STD_INPUT_HANDLE))
+         (rdir       (redirected-handle? (sys-get-std-handle hdl-type)))
+         (conv       (if rdir (if (or (= rmode 2) (= rmode 3)) #t #f) #t))
+         (crlf       (if rdir (if (or (= rmode 1) (= rmode 3)) #t #f) #f))
+         (ces2       (gauche-character-encoding)))
     ;; 文字エンコーディングが未指定のときは、コードページを取得して自動設定する
     (unless ces
-      (let1 cp (if stdin-flag (sys-get-console-cp) (sys-get-console-output-cp))
+      (let1 cp (if stdin-flag
+                 (sys-get-console-cp)
+                 (sys-get-console-output-cp))
         (case cp
           ((65001)
            (set! ces 'UTF-8)
@@ -198,37 +254,46 @@
     ;; リダイレクトありのときは Windows API は使用不可
     (if rdir (set! use-api #f))
     ;; 結果を多値で返す
-    (values conv crlf hdl-type ces ces2 use-api)))
+    (values conv crlf ces ces2 use-api)))
 
 
 
 ;; 標準入力の変換ポートの作成
 (define (make-msjis-stdin-port :optional (rmode 0) (ces '#f) (use-api #f))
-  (receive (conv crlf hdl-type ces ces2 use-api)
-      (get-msjis-param rmode STD_INPUT_HANDLE ces use-api #t)
+  (receive (conv crlf ces ces2 use-api)
+      (get-msjis-param rmode STD_INPUT_HANDLE ces use-api)
     (if conv
-      (make <virtual-input-port>
-        :getc (make-msjis-getc (standard-input-port) hdl-type ces ces2 use-api))
+      (rlet1 vport (make <virtual-input-port>)
+        (port-attribute-set! vport 'windows-console-conversion #t)
+        (let1 proc (make-msjis-getc (standard-input-port)
+                                    STD_INPUT_HANDLE ces ces2 use-api)
+          (set! (~ vport'getc) proc)))
       #f)))
 
 ;; 標準出力の変換ポートの作成
 (define (make-msjis-stdout-port :optional (rmode 0) (ces '#f) (use-api #f))
-  (receive (conv crlf hdl-type ces ces2 use-api)
-      (get-msjis-param rmode STD_OUTPUT_HANDLE ces use-api #f)
+  (receive (conv crlf ces ces2 use-api)
+      (get-msjis-param rmode STD_OUTPUT_HANDLE ces use-api)
     (if (or conv crlf)
-      (make <virtual-output-port>
-        :putc (make-msjis-puts (standard-output-port) conv crlf hdl-type ces ces2 use-api)
-        :puts (make-msjis-puts (standard-output-port) conv crlf hdl-type ces ces2 use-api))
+      (rlet1 vport (make <virtual-output-port>)
+        (port-attribute-set! vport 'windows-console-conversion #t)
+        (let1 proc (make-msjis-puts (standard-output-port) conv crlf
+                                    STD_OUTPUT_HANDLE ces ces2 use-api)
+          (set! (~ vport'putc) proc)
+          (set! (~ vport'puts) proc)))
       #f)))
 
 ;; 標準エラー出力の変換ポートの作成
 (define (make-msjis-stderr-port :optional (rmode 0) (ces '#f) (use-api #f))
-  (receive (conv crlf hdl-type ces ces2 use-api)
-      (get-msjis-param rmode STD_ERROR_HANDLE ces use-api #f)
+  (receive (conv crlf ces ces2 use-api)
+      (get-msjis-param rmode STD_ERROR_HANDLE ces use-api)
     (if (or conv crlf)
-      (make <virtual-output-port>
-        :putc (make-msjis-puts (standard-error-port) conv crlf hdl-type ces ces2 use-api)
-        :puts (make-msjis-puts (standard-error-port) conv crlf hdl-type ces ces2 use-api))
+      (rlet1 vport (make <virtual-output-port>)
+        (port-attribute-set! vport 'windows-console-conversion #t)
+        (let1 proc (make-msjis-puts (standard-error-port) conv crlf
+                                    STD_ERROR_HANDLE ces ces2 use-api)
+          (set! (~ vport'putc) proc)
+          (set! (~ vport'puts) proc)))
       #f)))
 
 
